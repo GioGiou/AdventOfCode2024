16f38746590a6e9de5ac6b044f1c9faea3c8073c
Jani <giogiou@github.com>
2024-12-06 19:59:09 +0100

    Day 6 part 2

--- Day6/README.md
+++ Day6/README.md
@@ -2,12 +2,14 @@
 
 Rešen dneva 6. 12. 2024
 
-Začetek: 8:17
+Začetek: 8:17 (2. del 18:55)
 
-Konec: 9:13 1. del,
+Konec: 9:13 1. del, 19:58
 
 # Rang
 
 Part 1: 20075
 
-Part 2: 1
+Part 2: 32585
+
+Algoritem drugega dela deluje v času O(n^4)+ binarno iskanje nad skipom
--- Day6/src/App.java
+++ Day6/src/App.java
@@ -118,6 +118,173 @@ public class App {
         }
         System.out.println("Answer 1: " + rez1);
         br.close();
+        br = new BufferedReader (new FileReader(".\\Day6\\src\\Test\\test2.txt"));
+        data = new ArrayList<String>();
+        int zacx=0;
+        int zacy=0;
+        int rez2=0;
+        f = true;
+        while (br.ready()) {
+            String line = br.readLine();
+            data.add(line);
+            if(line.contains("^")){
+                zacy=line.indexOf("^");
+                f=false;
+            }
+            else{
+                if(f){zacx++;}
+                
+            }
+        }
+        int skip = 0;
+        for (int i = 0;i<data.size();i++) {
+            for (int j = 0; j < data.get(i).length(); j++) {
+                String[] map = data.toArray(new String[]{});
+                if(map[i].charAt(j)=='#'){
+                    continue;
+                }
+                if(map[i].charAt(j)=='^'){
+                    continue;
+                }
+                StringBuilder myName = new StringBuilder(map[i]);
+                myName.setCharAt(j, 'O');
+                map[i]=myName.toString();
+                position = "UP";
+                x=zacx;
+                y=zacy;
+                boolean loop= false;
+                boolean O= false;
+                
+                int cout=0;
+                while(x>=0 && x<data.size() && y>=0 && y<data.get(0).length()){
+                    cout++;
+                    if(cout>map[i].length()*map.length){
+                        System.out.println("Line: "+i+"\nChar: "+j);
+                        skip++;
+                        break;    
+                    }
+                    if(cout%150==0){
+                        System.out.println("Count: "+cout);
+                    }
+                    
+                switch (position) {
+                    case "UP":
+                        if(x==0){
+                            x--;
+                            break;
+                        }
+                        if(map[x-1].charAt(y)=='O'){
+                            position="RIGHT";
+                            if(O){
+                                System.out.println("O: "+(x-1==i)+" "+(y==j));
+                                loop =true;
+                                break;
+                            }
+                            O=true;
+                            x++;
+                        }
+                        if(map[x-1].charAt(y)=='#'){
+                            position="RIGHT";
+                            x++;
+                        }
+                        x--;
+                        
+                        
+                        break;
+
+                    case "DOWN":
+
+                        if(x==data.size()-1){
+                            x++;
+                            break;
+                        }
+                        if(map[x+1].charAt(y)=='O'){
+                            position="LEFT";
+                            if(O){
+                                System.out.println("O: "+(x+1==i)+" "+(y==j));
+                                loop =true;
+                                break;
+                            }
+                            O=true;
+                            x--;
+
+                        } 
+                        if(map[x+1].charAt(y)=='#'){
+                            position="LEFT";
+                            x--;
+
+                        }
+                        x++;
+                        
+                        
+                        
+                        break;
+
+                    case "RIGHT":
+                        if(y==map[x].length()-1){
+                            y++;
+                            break;
+                        }
+                        if(map[x].charAt(y+1)=='O'){
+                            position="DOWN";
+                            if(O){
+                                System.out.println("O: "+(x==i)+" "+(y+1==j));
+                                loop =true;
+                                break;
+                            }
+                            O=true;
+                            y--;
+
+                        }   
+                        if(map[x].charAt(y+1)=='#'){
+                            position="DOWN";
+                            y--;
+
+                        }
+                        y++;
+                        
+                        
+                        break;
+
+                    case "LEFT":
+                        if(y==0){
+                            y--;
+                            break;
+                        }
+                        if(map[x].charAt(y-1)=='O'){
+                            position="UP";
+                            if(O){
+                                System.out.println("O: "+(x==i)+" "+(y-1==j));
+                                loop =true;
+                                break;
+                            }
+                            O=true;
+                            y++;
+
+                        } 
+                        if(map[x].charAt(y-1)=='#'){
+                            position="UP";
+                            y++;
+
+                        }
+                        
+                        y--;
+                        break;
+                
+                    default:
+                        return;
+                    }
+                    if(loop){
+                        System.out.println(i+", " +j);
+                        rez2++;
+                        break;
+                    }
+                }
+            }
+            
+        }
+        System.out.println("Answer 2: " +rez2+"Skip: "+skip);
+        br.close();
     }
 
 
