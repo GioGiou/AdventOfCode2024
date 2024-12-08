275558c30c1e074c5c8decdda2261fa31ebac5a5
Jani <giogiou@github.com>
2024-12-08 14:39:58 +0100

    Day 8 z uporabo Record

--- Day8/src/App.java
+++ Day8/src/App.java
@@ -6,9 +6,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class App {
+    public record Coordinate(int x, int y) {}
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader (new FileReader(".\\Day8\\src\\Test\\test2.txt"))){
-            HashMap<String,ArrayList<int[]>> data = new HashMap<>();
+            HashMap<String,ArrayList<Coordinate>> data = new HashMap<>();
             int i =0;
             int j =0;
             while (br.ready()) {
@@ -16,13 +17,13 @@ public class App {
                 for (String c : br.readLine().split("")) {
                     if(!c.equals(".")) {
                         if (data.keySet().contains(c)){
-                            ArrayList<int[]> temp = data.get(c);
-                            temp.add(new int[]{i,j});
+                            ArrayList<Coordinate> temp = data.get(c);
+                            temp.add(new Coordinate(i, j));
                             data.put(c,temp);
                         }
                         else{
-                            ArrayList<int[]> temp = new ArrayList<>();
-                            temp.add(new int[]{i,j});
+                            ArrayList<Coordinate> temp = new ArrayList<>();
+                            temp.add(new Coordinate(i, j));
                             data.put(c,temp);
                         }
                         System.out.println(i+", "+j);
@@ -34,46 +35,29 @@ public class App {
             System.out.println("size: "+i + ", "+j);
             int rez1 = 0;
             int rez2 = 0;
-            HashSet<int[]> rez = new HashSet<>();
-            HashSet<int[]> rezp2 = new HashSet<>();
+            HashSet<Coordinate> rez = new HashSet<>();
+            HashSet<Coordinate> rezp2 = new HashSet<>();
             for (String key : data.keySet()) {
-                ArrayList<int[]> antena = data.get(key);
-                
-                for ( int[] loc: antena) {
-                    
-                    
-                    for ( int[] loc2: antena) {
+                ArrayList<Coordinate> antena = data.get(key);                
+                for ( Coordinate loc: antena) {
+                    for (Coordinate loc2: antena) {
                         if(!loc.equals(loc2)){
-                            int x=loc[0]-loc2[0];
-                            int y=loc[1]-loc2[1];
-                            int ax = x + loc[0];
-                            int ay= y + loc[1];
-
-                            if(ax>=0 && ax <i&& ay>=0 &&ay<j){
-                                boolean add = true;
-                                for (int[] ks : rez) {
-                                     if(ks[0]==ax && ks[1]==ay){
-                                        add = false;
-                                     }
-                                }
-                                if(add){
-                                    rez.add(new int[]{ax,ay});
-                                }
+                            int x=loc.x()-loc2.x();
+                            int y=loc.y()-loc2.y();
+                            int ax = x + loc.x();
+                            int ay= y + loc.y();
+                            Coordinate anti  =  new Coordinate(ax, ay);
+                            if(anti.x()>=0 && anti.x() <i&& anti.y()>=0 &&anti.y()<j){
+                                rez.add(anti);
                                 rez1++;
                             }
-                            int k=0; 
-                            while (k*x+ loc[0]>=0 && k*x+ loc[0] <i&& k*y+ loc[1]>=0 &&k*y+ loc[1]<j){
-                                boolean add = true;
-                                for (int[] ks : rezp2) {
-                                     if(ks[0]==k*x+ loc[0] && ks[1]==k*y+ loc[1]){
-                                        add = false;
-                                     }
-                                }
-                                if(add){
-                                    rezp2.add(new int[]{k*x+ loc[0],k*y+ loc[1]});
-                                }
+                            int k=0;
+                            anti  =  new Coordinate(k*x+loc.x(), k*y+loc.y());
+                            while(anti.x()>=0 && anti.x() <i&& anti.y() >=0 && anti.y()<j){                                
+                                rezp2.add(anti);
                                 rez2++;
                                 k++;
+                                anti  =  new Coordinate(k*x+loc.x(), k*y+loc.y());
                             }
                         }
                     }
