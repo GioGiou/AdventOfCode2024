ed14db2f84d510e6f03448c7823e1d80f1660ba0
Jani <giogiou@github.com>
2024-12-11 20:48:25 +0100

    Popravek kode za part 2

--- Day11/README.md
+++ Day11/README.md
@@ -10,4 +10,4 @@ Konec: 8:42 1. del, N/A ista koda
 
 Part 1: 16386
 
-Part 2: 11257
+Part 2: 26322 Rešena s pomočjo https://github.com/jude253/AdventOfCode/blob/30bc014baa3eb94cfe29617d17dc889a0876c00a/src/advent_of_code/solutions/day_11.py#L19 in je bila reimplementirana v delu z komentarjem part2
--- Day11/src/App.java
+++ Day11/src/App.java
@@ -1,78 +1,47 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class App {
-    public static int[] ret;
-    
-    static class Run implements Runnable{
-        String stone;
-        int i;
-        Run(String stone, int i){
-            this.stone=stone;
-            this.i = i;
-        }
-        @Override
-        public void run() {
-            ArrayList<String> Data = new ArrayList<>();
-            Data.add(stone);
-            for (int x = 0; x < 43; x++) {
-                System.out.println(i+" je na "+x+ " in ima velikost "+Data.size());
-                ArrayList<String> newData = new ArrayList<>();
-                for (int j = 0; j < Data.size(); j++) {
-                    
-                    if(Long.parseLong(Data.get(j))==0){
-                        newData.add(j, "1");
-                    }
-                    else if(Data.get(j).length()%2==0){
-                        int half=Data.get(j).length()/2;
-                        String left = Long.parseLong(Data.get(j).substring(0,half))+"";
-                        String right = Long.parseLong(Data.get(j).substring(half))+"";
-                        newData.add(left);
-                        newData.add(right);
-                    }
-                    else{
-                        long temp = Long.parseLong(Data.get(j));
-                        String added = ""+(temp*2024);
-                        newData.add(added);
-                    }
-                }
-                Data = newData;
-                newData=null;
-
-            }
-
-            App.ret[i]=Data.size();
-        }
-    }
+    static List<String> newData;
+    static Map<String,Long> newDataMap;
     
     public static void main(String[] args) {
         try(BufferedReader br = new BufferedReader(new FileReader(".\\Day11\\src\\Test\\test2.txt"))) {
             String[] line = br.readLine().split(" ");
-            ArrayList<String> data = new ArrayList<>();
+            List<String> data = Collections.synchronizedList(new LinkedList<>());
+            Map<String,Long> dataMap = new HashMap<>();
             for (int i = 0; i < line.length; i++) {
                 data.add(line[i]);
+                if(dataMap.containsKey(line[i])){
+                    long temp = dataMap.get(line[i]);
+                    dataMap.put(line[i], temp+1);
+                }
+                else{
+                    dataMap.put(line[i], 1l);
+                }
             }
             //Part 1
-            for (int x = 0; x < 32; x++) {
-                ArrayList<String> newData = new ArrayList<>();
-                for (int i = 0; i < data.size(); i++) {
-                    if(Long.parseLong(data.get(i))==0){
-                        newData.add(i, "1");
+            for (int x = 0; x < 25; x++) {
+                newData = Collections.synchronizedList(new LinkedList<>());
+                for (String stone : data) {
+                    if(Long.parseLong(stone)==0){
+                        newData.add( "1");
                     }
-                    else if(data.get(i).length()%2==0){
-                        int half=data.get(i).length()/2;
-                        String left = Long.parseLong(data.get(i).substring(0,half))+"";
-                        String right = Long.parseLong(data.get(i).substring(half))+"";
+                    else if(stone.length()%2==0){
+                        int half=stone.length()/2;
+                        String left = Long.parseLong(stone.substring(0,half))+"";
+                        String right = Long.parseLong(stone.substring(half))+"";
                         newData.add(left);
                         newData.add(right);
                     }
                     else{
-                        long temp = Long.parseLong(data.get(i));
+                        long temp = Long.parseLong(stone);
                         String added = ""+(temp*2024);
                         newData.add(added);
                     }
@@ -82,21 +51,59 @@ public class App {
                 System.out.println(x+". Size="+data.size());
             }
             //Part2
-            try (ExecutorService myExecutor = Executors.newVirtualThreadPerTaskExecutor()) {
-                Future<?>[] t = new Future[data.size()];
-                ret = new int[data.size()];
-                for (int i = 0; i < data.size(); i++) {
-                    t[i] = myExecutor.submit(new Run(data.get(i),i));
+            for (int x = 1; x <= 75 ; x++) {
+                newDataMap = new HashMap<>();
+                for (Map.Entry<String, Long> map: dataMap.entrySet()) {                
+                    String stone = map.getKey();
+                    long numb = map.getValue();
+                    if(Long.parseLong(stone)==0){
+                        if(newDataMap.containsKey("1")){
+                            long temp = newDataMap.get("1");
+                            newDataMap.put("1", temp+numb);
+                        }
+                        else{
+                            newDataMap.put("1", numb);
+                        }
+                    }
+                    else if(stone.length()%2==0){
+                        int half=stone.length()/2;
+                        String left = Long.parseLong(stone.substring(0,half))+"";
+                        String right = Long.parseLong(stone.substring(half))+"";
+                        if(newDataMap.containsKey(left)){
+                            Long temp = newDataMap.get(left);
+                            newDataMap.put(left, temp+numb);
+                        }
+                        else{
+                            newDataMap.put(left, numb);
+                        }
+                        if(newDataMap.containsKey(right)){
+                            Long temp = newDataMap.get(right);
+                            newDataMap.put(right, temp+numb);
+                        }
+                        else{
+                            newDataMap.put(right, numb);
+                        }
+                    }
+                    else{
+                        long temp = Long.parseLong(stone);
+                        String added = ""+(temp*2024);
+                        if(newDataMap.containsKey(added)){
+                            long temp1 = newDataMap.get(added);
+                            newDataMap.put(added, temp1+numb);
+                        }
+                        else{
+                            newDataMap.put(added, numb);
+                        }
+                    }
                 }
-                long rez2 = 0;
-                for (int i = 0; i < line.length; i++) {
-                    t[i].get();
-                    rez2 = rez2 + ret[i];
+                dataMap = newDataMap;
+                newDataMap=null;
+                
+                long rez =0;
+                for (Map.Entry<String, Long> map: dataMap.entrySet()) {
+                    rez = rez + map.getValue(); 
                 }
-                System.out.println("Answer 2: "+rez2);
-            }
-            catch (Exception e){
-                e.printStackTrace();
+                System.out.println(x+". Size="+dataMap.size()+", Total="+rez);
             }
 
             
